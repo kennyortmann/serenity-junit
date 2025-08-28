cebb12642049e2c4704d963a26c11db94edb8ffb
Kenny Ortmann <kenny.ortmann@wwt.com>
2025-08-28 11:37:37 -0500

    Adding a simple test. ko

---
+++ src/test/java/starter/duckduckgo/WhenSearchingByKeyword.java
@@ -0,0 +1,27 @@
+package starter.duckduckgo;
+
+import net.serenitybdd.annotations.Managed;
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SerenityJUnit5Extension.class)
+public class WhenSearchingByKeyword {
+
+    @Managed(driver = "chrome")
+    WebDriver driver;
+
+    NavigateActions navigate;
+    SearchActions search;
+    SearchResultSidebar searchResultSidebar;
+
+    @Test
+    void theKeywordShouldAppearInTheResultsSidebar() {
+        navigate.toTheDuckDuckGoSearchPage();
+        search.byKeyword("Cucumber");
+        assertThat(searchResultSidebar.heading()).isEqualTo("Searches related to Cucumber");
+    }
+}
